29583d882457a8e963d7c380d02923a3da5d2442
amberbrown4 <zasalarian2000@gmail.com>
2020-09-28 18:37:28 +0330

    Added 14 books of Charles Dickens

--- gutenberg_1.r
+++ gutenberg_1.r
@@ -4,51 +4,67 @@ library(tidytext)
 library(magrittr)
 library(ggplot2)
 library(dplyr)
+library(stringr)
 
-to_download <- gutenberg_metadata%>%
-  filter((author == "Dickens, Charles")&(!is.na(gutenberg_bookshelf))&has_text&(language == "en"))
-book_id<- as.vector(unlist(to_download$gutenberg_id))
-books_tbl <- gutenberg_download(book_id, meta_fields = "title")
+#to_download <- gutenberg_metadata%>%
+#  filter((author == "Dickens, Charles")&(!is.na(gutenberg_bookshelf))&has_text&(language == "en"))
+#book_id<- as.vector(unlist(to_download$gutenberg_id))
+#books_tbl <- gutenberg_download(book_id, meta_fields = "title")
 
 # download all books from Charles Dickens
+books <- c("The Pickwick Papers","Oliver Twist",
+           "Nicholas Nickleby","The Old Curiosity Shop",
+           "Barnaby Rudge","Martin Chuzzlewit",
+           "Dombey and Son","David Copperfield",
+           "Bleak House","Hard Times",
+           "Little Dorrit","A Tale of Two Cities",
+           "Great Expectations","Our Mutual Friend",
+           "The Mystery of Edwin Drood")
 #dickes <- gutenberg_works(author == "Dickens, Charles") %>%
 #  gutenberg_download(meta_fields = "title")
-#aliceref <-gutenberg_works(title == "A Christmas Carol")
-#books_tbl <- gutenberg_download(aliceref$gutenberg_id) %>% gutenberg_strip()
-tidytext <- data_frame(line = 1:nrow(books_tbl), text = books_tbl$text) %>%
-  unnest_tokens(word, text) %>%
-  anti_join(stop_words) %>%
-  count(word, sort = TRUE)
-tidytext<-head(tidytext,20)
-ggplot(tidytext,aes(x=word, y=n))+
-geom_bar(stat="identity", fill="red")
-  #geom_bar(height=head(tidytext,10)$n, names.arg=head(tidytext,10)$word, xlab="Mots", ylab="Fréquence", col="#973232", main="Alice in Wonderland")
+books_id = c()
+for(book in books){
+  id <-gutenberg_works(title == book)[[1]]
+  books_id<-c(id,books_id)
+}
+books_tbl <- gutenberg_download(books_id,meta_fields = "title") 
 
-#words <- plato %>% unnest_tokens(word,text)
+#(((())))
+#tidytext <- data_frame(line = 1:nrow(books_tbl), text = books_tbl$text) %>%
+# unnest_tokens(word, text) %>%
+#  anti_join(stop_words) %>%
+# count(word, sort = TRUE)
+#t <- data_frame(line = 1:nrow(books_tbl), text = books_tbl$text) 
 
-#books <- list(
- # A_Christmas_Carol_in_Prose = books_tbl[title=="A Christmas Carol in Prose; Being a Ghost Story of Christmas"],
-#  A_Tale_of_Two_Cities = books_tbl[title=="A Tale of Two Cities"],
- # The_Mystery_of_Edwin_Drood = books_tbl[title=="The Mystery of Edwin Drood"],
-#  The_Pickwick_Papers = books_tbl[title=="The Pickwick Papers"],
- # The_Haunted_Man_and_the_Ghosts_Bargain = books_tbl[title=="The Haunted Man and the Ghost's Bargain"],
-#  The_Cricket_on_the_Hearth =books_tbl[title=="The Cricket on the Hearth: A Fairy Tale of Home"],
- # A_Childs_History_of_England = books_tbl[title=="A Child's History of England"],
-#  David_Copperfield = books_tbl[title=="David Copperfield"],
- # Hunted_Down= books_tbl[title=="Hunted Down: The Detective Stories of Charles Dickens"],
-  #Holiday_Romance = books_tbl[title=="Holiday Romance"],
-#  Barnaby_Rudge = books_tbl[title=="Barnaby Rudge: A Tale of the Riots of 'Eighty"],
- # Martin_Chuzzlewit = books_tbl[title=="Martin Chuzzlewit"],
-  #Great_Expectations = books_tbl[title=="Great Expectations"],
-#  Some_Christmas_Stories = books_tbl[title=="Some Christmas Stories"],
- # A_Christmas_Carol_1 = books_tbl[gutenburg_id==19337],
-  #A_Christmas_Carol_2 = books_tbl[gutenburg_id==19505],
-  #The_Cricket_on_the_Hearth = books_tbl[title=="The Cricket on the Hearth"],
-  #The_Magic_Fishbone  = books_tbl[gutenberg_id==23344],
-  #The_Trial_of_William_Tinkling  = books_tbl[gutenberg_id==23452],
-  #Captain_Boldheart = books_tbl[gutenberg_id==23765],
-  #A_Christmas_Carol = books_tbl[title=="A Christmas Carol"]
-  #) %>%
-  #ldply(rbind) %>%
-#  gather(key = 'chapter', value = 'text', -book) %>%
- # filter(!is.na(text)) 
+#tidytext['n'] <- sqrt(tidytext['n'])
+#tidytext<-head(tidytext,20)
+#ggplot(tidytext,aes(x=word, y=n))+
+#  geom_bar(stat="identity", fill="red")
+
+#wordcloud
+library(devtools) 
+#install_github("lchiffon/wordcloud2")
+library(wordcloud2)
+#figPath = system.file("dickens.jpg",package = "wordcloud2")
+#wordcloud2(data = tidytext, size = 1.5,color='random-light', backgroundColor="black")
+
+
+#characters name
+#tidytext_2 <- data_frame(line = 1:nrow(books_tbl), text = books_tbl$text, title=books_tbl$title) %>%
+#  unnest_tokens(word, text) %>%
+#  anti_join(stop_words)%>%
+#  group_by(title)%>%
+# count(word, sort = TRUE)
+
+#tidytext_3<-tidytext_2%>%
+#  filter(str_detect(word,"[A-Z][a-z]+"))
+#tidytext_4<-tidytext_2%>%
+#  filter(str_detect(word,"[A-Za-z]+"))
+
+#names <-names[!str_to_lower(names)%in%words]
+
+#tidytext_2<- tidytext_2%>%
+#  group_by(title,word)%>%
+# mutate(n_word = n())
+#tidytext_2<- distinct(tidytext_2)
+#count(word, sort = TRUE)
