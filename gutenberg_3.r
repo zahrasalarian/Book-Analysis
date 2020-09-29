27ccc779cb33fdbaa550ed8fb51de7e0e4091094
amberbrown4 <zasalarian2000@gmail.com>
2020-09-29 09:29:06 +0330

    divided table into 200 pieces

--- gutenberg_3.r
+++ gutenberg_3.r
@@ -5,6 +5,7 @@ library(magrittr)
 library(ggplot2)
 library(dplyr)
 library(stringr)
+require(tidyr)
 books <- c("Les Misérables, v. 1/5: Fantine",
            "Les Misérables, v. 2/5: Cosette",
            "Les Misérables, v. 3/5: Marius",
@@ -16,4 +17,13 @@ for(book in books){
   books_id<-c(id,books_id)
 }
 books_tbl <- gutenberg_download(books_id,meta_fields = "title") 
-
+#divide into 200 pieces
+tidytext <- data_frame(line = 1:nrow(books_tbl), text = books_tbl$text) %>%
+  unnest_tokens(word, text) %>%
+  anti_join(stop_words)
+tidytext$id <- seq.int(nrow(tidytext))
+#n<- 200
+#pieces <- split(tidytext, rep(1:n, length.out=nr))
+#pieces<-tidytext[, parallel_operation(.SD), by = parallel.id] 
+tidytext$group <- as.numeric(cut(tidytext$id, 200))
+#bing <- get_sentiments("bing")
